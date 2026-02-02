48916da261b60d0d3f478683ad2a4081fbb44779
Artem Syrnikov <114866139+syrnikov@users.noreply.github.com>
2026-02-02 15:01:24 +0200

    init

---
+++ cdcase/cdcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  cdcaseApp.swift
+//  cdcase
+//
+//  Created by Aaron Syrnikov on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct cdcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
